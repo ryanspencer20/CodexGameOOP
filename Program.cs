4876271abe957df0bc44155beb0f8e8cf860e623
ryanspencer20 <ryanspencer20@gmail.com>
2026-03-31 18:48:12 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Program to run the Codex Game
+using System;
+namespace CodexGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Codex Game!");
+            Console.WriteLine("Please enter your name:");
+            string playerName = Console.ReadLine();
+            Console.WriteLine($"Hello, {playerName}! Let's start the game.");
+        }
+    }
+}
